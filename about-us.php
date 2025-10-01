58f5965d2d360f5884c5b98fd83989dc0b5a219d
josephfoster05 <josephfoster2005@gmail.com>
2025-10-01 15:25:39 +0100

    about us skeleton

---
+++ about-us.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php include 'header-footer.php'; ?>
+<head>
+    <meta charset="UTF-8">
+    <title>About Us - SustainWear</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+<body>
+    <main>
+            <h2>About Us</h2>
+            <p>Connor Barton</p>
+            <p>Andy Liu</p>
+            <p>Joe Foster</p>
+            <p>Josh. just josh</p>
+    <main>
+</html>
