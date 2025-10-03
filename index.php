344c53716ee656f4a4aad3c6a1bd3d93477cfbfc
DarkFoxJnr <darkfoxjnr123@gmail.com>
2025-10-03 14:34:03 +0100

    added in stylesheet and class for the main text

--- index.php
+++ index.php
@@ -5,13 +5,14 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="shared/styles.css">
 </head>
 
 <body>
     <?php render_header(); ?>
     <main>
         <section>
-            <h2>Welcome to SustainWear!</h2>
+            <h2 class="main-text">Welcome to SustainWear!</h2>
             
             <!-- Main content goes here -->
         </section>
